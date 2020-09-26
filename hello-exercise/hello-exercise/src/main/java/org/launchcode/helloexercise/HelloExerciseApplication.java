cda64ccba879175d2ffbaeff1400989c2908e703
sighmahn <sighmahn@yahoo.com>
2020-09-26 18:55:46 -0500

    something to see

---
+++ hello-exercise/hello-exercise/src/main/java/org/launchcode/helloexercise/HelloExerciseApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.helloexercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloExerciseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloExerciseApplication.class, args);
+	}
+
+}
---
+++ hello-exercise/hello-exercise/src/test/java/org/launchcode/helloexercise/HelloExerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package org.launchcode.helloexercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloExerciseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
