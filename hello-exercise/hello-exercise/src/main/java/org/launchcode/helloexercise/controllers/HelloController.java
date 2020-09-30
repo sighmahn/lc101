3f15a108edf6a0fdcab655e7b95ed3ec6659784d
sighmahn <sighmahn@yahoo.com>
2020-09-30 11:22:12 -0500

    the last day of September

--- hello-exercise/hello-exercise/src/main/java/org/launchcode/helloexercise/controllers/HelloController.java
+++ hello-exercise/hello-exercise/src/main/java/org/launchcode/helloexercise/controllers/HelloController.java
@@ -1,8 +1,50 @@
 package org.launchcode.helloexercise.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
 
+    @GetMapping("helloform")
+    @ResponseBody
+    public String helloForm() {
+        return "<form method='POST' action='hello'>" +
+                "<input type='text' name='name'>" +
+                "<select name='language'>" +
+                    "<option value='english'>English</option>" +
+                    "<option value='spanish'>Spanish</option>" +
+                    "<option value='dutch'>Dutch</option>" +
+                    "<option value='ukrainian'>Ukrainian</option>" +
+                    "<option value='latin'>Latin</option>" +
+                "</select>" +
+                "<input type='submit' value = 'Greet Me'>" +
+                "</form>";
+    }
+
+    //@PostMapping("hello")
+    @GetMapping("hello")
+    @ResponseBody
+    public String sayHello(@RequestParam String name, @RequestParam String language) {
+        return createMessage(name, language);
+    }
+
+    public static String createMessage(String name, String language) {
+        String result = "";
+        if(language.equals("english")) {
+            result = "Hello, " + name;
+        } else if (language.equals("spanish")) {
+            result = "Hola, " + name;
+        } else if (language.equals("dutch")) {
+            result = "Hallo, " + name;
+        } else if (language.equals("ukrainian")) {
+            result = "Pryvit, " + name;
+        } else if (language.equals("latin")) {
+            result = "Salve, " + name;
+        }
+        return result;
+    }
 }
--- skills-tracker/skills-tracker/src/main/java/org/launchcode/skillstracker/controllers/SkillsController.java
+++ skills-tracker/skills-tracker/src/main/java/org/launchcode/skillstracker/controllers/SkillsController.java
@@ -2,14 +2,16 @@ package org.launchcode.skillstracker.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class SkillsController {
 
-    @GetMapping("/")
+    @GetMapping
     @ResponseBody
-    public String skillslist() {
+    public String skillsList() {
         return "<h1>Skills Tracker</h1>" +
                 "<h2>These are some skills that I want to learn</h2>" +
                 "<ol>" +
@@ -23,21 +25,37 @@ public class SkillsController {
     @ResponseBody
     public String skillForm() {
         return "<form action='/form' method='post'>" +
-                "<input type='text' name='name'>" +
+                "<label>Name</label><br />" +
+                "<input type='text' name='name'><br />" +
+                "<label>Favorite</label><br />" +
                 "<select name='skill0'>" +
                 "<option value='Java'>Java</option>" +
                 "<option value='Python'>Python</option>" +
-                "<option value='Javascript'>" +
-                "</select>" +
-                "<select name='skill1'>" +
-                "<option value='Java'>" +
-                "<option value='Python'>" +
                 "<option value='Javascript'>Javascript</option>" +
-                "</select>" +
+                "</select><br />" +
+                "<label>Second Favorite</label><br />" +
+                "<select name='skill1'>" +
+                "<option value='Java'>Java</option>" +
+                "<option value='Python'>Python</option>" +
+                "<option value='Javascript'>Javascript</option>" +
+                "</select><br />" +
+                "<label>Third Favorite</label><br />" +
                 "<select name='skill2'>" +
-                "<option value='Java'>" +
-                "<option value='Python'>" +
-                "<option value='Javascript'>" +
-                "</select>" +
+                "<option value='Java'>Java</option>" +
+                "<option value='Python'>Python</option>" +
+                "<option value='Javascript'>Javascript</option>" +
+                "</select><br />" +
+                "<input type='submit' value='Submit'>" +
+                "</form>";
+    }
+    @PostMapping("/form")
+    @ResponseBody
+    public String processForm(@RequestParam String name, @RequestParam String skill0, @RequestParam String skill1, @RequestParam String skill2) {
+        return "<h1>" + name + "</h1><br>" +
+                "<ol>" +
+                "<li>" + skill0 + "</li>" +
+                "<li>" + skill1 + "</li>" +
+                "<li>" + skill2 + "</li>" +
+                "</ol>";
     }
 }
