7e3afbb44e125a8df41a8e2ce5e7dda8ffe47411
sighmahn <sighmahn@yahoo.com>
2020-10-04 19:40:41 -0500

    Sunday4th2020

---
+++ coding-events/coding-events/src/test/java/org/LaunchCode/codingevents/CodingEventsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.LaunchCode.codingevents;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodingEventsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
