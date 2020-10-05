6a3c18cd844d7c63f8aa541e1d25908ade7b3166
sighmahn <sighmahn@yahoo.com>
2020-10-05 11:51:17 -0500

    MondayThe5th2020

---
+++ coding-events/coding-events/src/main/java/org/LaunchCode/codingevents/controllers/HomeController.java
@@ -0,0 +1,14 @@
+package org.LaunchCode.codingevents.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping
+    public String index() {
+        return "index";
+    }
+
+}
